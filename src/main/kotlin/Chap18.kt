bcea651591c5251fb5761c19fc0e0012cf92d68a
itc-s23012 <s23012@std.it-college.ac.jp>
2024-05-15 09:51:15 +0900

    Chap18終了

--- src/main/kotlin/Chap18.kt
+++ src/main/kotlin/Chap18.kt
@@ -1,5 +1,42 @@
 package org.example
 
-fun main(args: Array<String>) {
+import kotlin.time.measureTime
 
+fun main(args: Array<String>) {
+   run {
+        val s: Set<String> = setOf("あ", "い", "う", "え", "お")
+        println(s)
+    }
+
+    run {
+        println("----18.2----")
+        val s: MutableSet<String> = mutableSetOf("あ", "い", "う", "え", "お")
+        s += "か"
+        s += "き"
+        s += "く"
+        s += "い"
+        s += "お"
+        s += "か"
+        println(s)
+    }
+
+    val charlist = 'a' .. 'z'
+    val searchCharList = mutableListOf('z', 'x', 'c', 'v', 'b', 'n', 'm')
+    val serchCharset = mutableSetOf('z', 'x', 'c', 'v', 'b', 'n', 'm')
+    val count = 1_000_000_000L
+    val listTime = measureTime {
+        for (i in 0 ..<count){
+            val target = charlist.shuffled()[0]
+            println(" 「${target}」は存在する？ : ${target in searchCharList}")
+        }
+    }
+    val setTime = measureTime {
+        for (i in 0 ..<count) {
+            val target = charlist.shuffled()[0]
+            println(" 「${target}」は存在する？ : ${target in searchCharList}")
+        }
+    }
+
+    println("Listでかかった時間: $listTime")
+    println(" Setデカかった時間: $setTime")
 }
