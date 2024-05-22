8405d7a15d729d11783a5aa86ecf0a24ae5e9101
itc-s23012 <s23012@std.it-college.ac.jp>
2024-05-22 10:06:46 +0900

    Chap23終了

---
+++ src/main/kotlin/Chap23.kt
@@ -0,0 +1,47 @@
+package org.example
+
+fun main(args: Array<String>) {
+    run {
+        println("----23.4----")
+        val p = Person3()
+    }
+    run {
+        println("----23.5----")
+        val p1 = Person3()
+        p1.name = "タケシ"
+        p1.age = 5
+        p1.dream = "警察官"
+
+        val p2 = Person3()
+        p2.name = "ケンタ"
+        p2.age = 6
+        p2.dream = "医者"
+
+        val p3 = Person3()
+        p3.name = "ユミ"
+        p3.age = 8
+        p3.dream = "先生"
+
+        p1.greet()
+        p2.greet()
+        p3.greet()
+    }
+}
+
+class Person1
+
+class Person2 {
+    var name = ""
+    var age = 0
+}
+
+// プロパティとメソッド(関数)を定義したクラス
+class Person3 {
+    var name = ""
+    var age = 0
+    var dream = ""
+
+    fun greet() {
+        println("こんにちは、私の名前は ${name}です。年齢は、${age}歳です。夢は、${dream}です。")
+    }
+}
